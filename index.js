08602b79dfa9600d7adc76f7947d8508e06257b0
OleksandrYatskiv <yatsk1660@gmail.com>
2023-04-26 21:48:39 +0200

    Finished work

---
+++ index.js
@@ -0,0 +1,7 @@
+firstNumber = +prompt('Enter first number');
+secondNumber = +prompt('Enter second number');
+thirdNumber = +prompt('Enter third number');
+
+averageValue = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(`The average value is ${averageValue}`);
